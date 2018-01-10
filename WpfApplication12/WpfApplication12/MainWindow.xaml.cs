c4fc140a76f120a87044336a4705355975f1e7a0
pjastr <24464854+pjastr@users.noreply.github.com>
2018-01-10 19:08:43 +0100

    Add files via upload

---
+++ WpfApplication12/WpfApplication12/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication12
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Osoba osoba1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            osoba1 = new Osoba();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //string tekst = Data.Text;
+            //DateTime data;
+            //if (DateTime.TryParse(tekst, out data))
+            //{
+            //    osoba1.UstawDane(Imie.Text, Nazwisko.Text, data);
+            //    Opis.Text = osoba1.ToString();
+            //}
+            //else
+            //{
+            //    MessageBox.Show("błędna data");
+            //}
+            osoba1.UstawDane(Imie.Text, Nazwisko.Text, Data2.SelectedDate.Value.Date);
+            Opis.Text = osoba1.ToString();
+                
+        }
+    }
+}
---
+++ WpfApplication12/WpfApplication12/Osoba.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication12
+{
+    internal class Osoba
+    {
+        private string imie;
+        private string nazwisko;
+        private DateTime dataUrodzenia;
+
+        public Osoba()
+        {
+
+        }
+
+        public void UstawDane(string imie, string nazwisko, DateTime dataUrodzenia)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.dataUrodzenia = dataUrodzenia;
+        }
+
+        public override string ToString()
+        {
+            return imie+Environment.NewLine+nazwisko+Environment.NewLine+dataUrodzenia.ToString("dd.MM.yyyy");
+        }
+    }
+}
